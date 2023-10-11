4ee741dd888042632178bba108111dca65f123a6
Da-achraf <achraf.boukir01@gmail.com>
2023-10-12 00:37:47 +0100

    Last Upadte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {PaginationService} from "./pagination.service";
-import {BehaviorSubject, Observable, of, switchMap} from "rxjs";
+import {BehaviorSubject, Observable, of, switchMap, tap} from "rxjs";
 import {faGear} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
@@ -14,17 +14,22 @@ export class AppComponent {
 
   currentPage$ = new BehaviorSubject<number>(1);
   data$ = this.currentPage$.pipe(
-    switchMap(page => this.paginationService.getPages(page))
+    switchMap(page => this.paginationService.getPages(page)),
+    tap(() => this.loading$.next(false))
   );
+
+  loading$ = new BehaviorSubject<boolean>(true);
 
   constructor(private paginationService: PaginationService) {}
 
   onNext(){
+    this.loading$.next(true)
     this.currentPage$.next(this.currentPage$.value + 1)
   }
 
   onPrevious(){
     if (this.currentPage$.value > 1){
+      this.loading$.next(true)
       this.currentPage$.next(this.currentPage$.value - 1)
     }
   }
